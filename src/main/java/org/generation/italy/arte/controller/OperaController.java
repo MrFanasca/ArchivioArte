1e779c0733937c21787839aa2e07a96f7199927f
Fanasca Fabrizio <fabri.taz5@live.it>
2024-02-03 14:38:16 +0100

    aggiunto metodo home

--- src/main/java/org/generation/italy/arte/controller/OperaController.java
+++ src/main/java/org/generation/italy/arte/controller/OperaController.java
@@ -3,6 +3,7 @@ package org.generation.italy.arte.controller;
 import org.generation.italy.arte.repository.OperaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,4 +12,10 @@ public class OperaController {
 
 	@Autowired
 	OperaRepository operaRepository;
+	
+	@GetMapping()
+	public String index () {
+		
+		return "opera/home";
+	}
 }
